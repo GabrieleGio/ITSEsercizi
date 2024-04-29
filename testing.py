a02db379b62b421c5f52358ba3b87850ba31785d
Gabriele Giovannangeli <giovannangeligabriele@gmail.com>
2024-04-29 18:00:13 +0200

    sync

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
-#prova
+print()
+l=[]
+dizionario = {"Chiave1":"Valore1", "Chiave2":"Valore2", "Chiave3":"Valore3"}
+print(dizionario)
+print()
+
+for chiave,valore in dizionario.items():
+    print(chiave)
+    print(valore)
+    l.append(valore)
+print(l)
