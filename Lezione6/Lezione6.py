d65bea25be53d8bdd82e5e63f2a952e4568c5d35
GabrieleGio <giovannangeligabriele@gmail.com>
2024-05-07 12:21:53 +0200

    commit

--- Lezione6/Lezione6.py
+++ Lezione6/Lezione6.py
@@ -33,3 +33,25 @@ for person in people:
 
 
 print(f"L'età più giovane è di {youngestname}, {youngestage} anni")
+print(person)
+
+print()
+print()
+
+class Student:
+    def __init__(self,name,age,studyProgram):
+        self.name = name
+        self.age = age
+        self.studyProgram = studyProgram
+
+    def printInfo(self):
+        return print(f"Il nome è {self.name}, l'età è {self.age} anni e il programma di studi è {self.studyProgram} ")
+        
+    
+myself = Student("Gabriele",20,"Cloud")
+left_neighbour = Student("Marco",22,"FullStack Developer")
+right_neighbour = Student("Francesco",23,"Cybersecurity")
+
+myself.printInfo()
+myself.age = 25
+    
