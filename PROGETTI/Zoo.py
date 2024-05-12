5e43c62f826ac5e8fe78df69aacf3a8c468e6ee3
Gabriele Giovannangeli <giovannangeligabriele@gmail.com>
2024-05-12 23:30:15 +0200

    commit

--- PROGETTI/Zoo.py
+++ PROGETTI/Zoo.py
@@ -4,10 +4,12 @@ class Zoo:
         self.zoo_keepers = zoo_keepers
 
     def __str__(self):
-        print("Animali nel recinto:")
-        for fence in self.fences:
-            for animal in 
-
+        zoo_str = "Zoo: \n"
+        for i,recinto in enumerate(self.fences,1):
+            zoo_str += f"Recinto {i}:\n"
+            for animale in recinto.animals:
+                zoo_str += f"-{animale.name}\n"
+        return zoo_str
 
 
     def describe_zoo():
@@ -37,9 +39,9 @@ class ZooKeeper:
         self.id = id
 
     def add_animal(self, animal: Animal, fence: Fence):
-        if animal not in self.animals:
-            if animal.preferred_habitat == fence.habitat
-            self.animals.append(animal)
+        if animal not in fence.animals:
+            if animal.preferred_habitat == fence.habitat:
+                fence.animals.append(animal)
     
     def remove_animal(self, animal: Animal, fence: Fence):
         pass
@@ -52,6 +54,18 @@ class ZooKeeper:
 
 
 ################################# ZONA TEST (CANCELLARE ALLA FINE)#######################################
-fence = Fence()
-tigre = Animal("Tigre","Panthera Tigris",3,110,30,"Savana",100)
+zoo1 = Zoo()
+recinto1 = Fence(50,35,"Savana")
+recinto2 = Fence(100,20,"Giungla")
 
+######## !!! ########
+zoo1.fences.append(recinto1)
+zoo1.fences.append(recinto2)
+######## !!! ########
+
+tigre = Animal("Tigre","Panthera Tigris",3,110,30,"Savana",100)
+scimmia = Animal("Scimmia","boh",4,50,10,"Giungla",100)
+guardiano1 = ZooKeeper("Mario","Rossi",1312)
+guardiano1.add_animal(tigre,recinto1)
+guardiano1.add_animal(scimmia,recinto2)
+print(zoo1.__str__())
