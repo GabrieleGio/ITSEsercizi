f83f9bad5c4e16a6fefae15b9fe2ac1900d622a3
Nio4124 <antoniozottola41204@gmail.com>
2024-05-27 18:49:45 +0200

    vedere calcola_tart

--- PROGETTI/TartarugaLepre.py
+++ PROGETTI/TartarugaLepre.py
@@ -8,30 +8,46 @@ print(percorso)
 
 def calcola_tartaruga(percorso:list):
     numero_random = random.randint(1,10)
-
-    #Passo veloce
-    if 1 <= numero_random <= 5:
-       nuovoindice = percorso.index(tartaruga) + 3
-       percorso.pop(tartaruga)
-       percorso.insert(nuovoindice,tartaruga)
-    
-    #Scivolata
-    elif 6 <= numero_random <= 7:
-        if percorso.index(tartaruga) - 6 < 0:
-            percorso.pop(tartaruga)
+    if tartaruga not in percorso:
+        #Passo veloce
+        if 1 <= numero_random <= 5:
+            percorso.pop(0)
+            percorso.insert(0,"_")
+            percorso.pop(3)
+            percorso.insert(3, tartaruga)
+        
+        #Scivolata
+        elif 6 <= numero_random <= 7:
             percorso.insert(0,tartaruga)
-        else:
-            nuovoindice = percorso.index(tartaruga) - 6
-            percorso.pop(tartaruga)
+        
+        #Passo lento
+        elif 8 <= numero_random <= 10:
+            percorso.insert(1,tartaruga)
+    else:
+        #Passo veloce
+        if 1 <= numero_random <= 5:
+            nuovoindice = percorso.index(tartaruga) + 3
+            percorso.pop(percorso.index(tartaruga))
             percorso.insert(nuovoindice,tartaruga)
-    
-    #Passo lento
-    elif 8 <= numero_random <= 10:
-       nuovoindice = percorso.index(tartaruga) + 1
-       percorso.pop(tartaruga)
-       percorso.insert(nuovoindice,tartaruga)
+        
+        #Scivolata
+        elif 6 <= numero_random <= 7:
+            if percorso.index(tartaruga) - 6 < 0:
+                percorso.pop(percorso.index(tartaruga))
+                percorso.insert(0,tartaruga)
+            else:
+                nuovoindice = percorso.index(tartaruga) - 6
+                percorso.pop(percorso.index(tartaruga))
+                percorso.insert(nuovoindice,tartaruga)
+        
+        #Passo lento
+        elif 8 <= numero_random <= 10:
+            nuovoindice = percorso.index(tartaruga) + 1
+            percorso.pop(percorso.index(tartaruga))
+            percorso.insert(nuovoindice,tartaruga)
     return percorso
 
-
-
-while True:
+print("#########################################")
+print(len(percorso))
+calcola_tartaruga(percorso)
+print(percorso)
