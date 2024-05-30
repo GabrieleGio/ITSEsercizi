9cc4c97e8245980f0b2a68540fa7c69dfd3aaaee
Nio4124 <antoniozottola41204@gmail.com>
2024-05-30 16:32:40 +0200

    meteo

--- PROGETTI/TartarugaLepre.py
+++ PROGETTI/TartarugaLepre.py
@@ -1,21 +1,25 @@
 import random
 
-
 def visualizza_posizioni(pos_tartaruga, pos_lepre):
     percorso = ['-'] * 70
     percorso[pos_tartaruga - 1] = 'T'
     percorso[pos_lepre - 1] = 'H'
-    if pos_tartaruga == pos_lepre:
+    if pos_tartaruga == pos_lepre and pos_tartaruga != 1:
         percorso[pos_tartaruga - 1] = 'OUCH!!!'
+    elif pos_tartaruga == pos_lepre and pos_tartaruga == 1:
+        percorso[pos_tartaruga - 1] = 'T and H'
     print(percorso)
 
 
 def muovi_tartaruga():
     i = random.randint(1, 10)
+    #Passo veloce
     if 1 <= i <= 5:
         return 3
+    #Scivolata
     elif 6 <= i <= 7:
         return -6
+    #Passo lento
     else:
         return 1
 
@@ -34,22 +38,54 @@ def muovi_lepre():
 
 pos_tartaruga = 1
 pos_lepre = 1
-
-
+tick = 0
+tickpioggia = 0
+cambiamentometeo = False
+meteo = "Sereno"
 print("BANG !!!!! AND THEY'RE OFF !!!!!")
 
 while pos_tartaruga < 70 and pos_lepre < 70:
 
-    mosse = {'tartaruga': muovi_tartaruga(), 'lepre': muovi_lepre()}
+    #Controllo meteo
+    if meteo == "Pioggia" and tickpioggia == 10:
+        meteo = "Sereno"
+        print("!!! NOW IT'S SUNNY !!!")
+        cambiamentometeo = True
+        tickpioggia = 0
+
+
+    #Inizio pioggia
+    if tick % 10 == 0 and tick != 0 and cambiamentometeo == False:
+        meteo = "Pioggia"
+        print("!!! IT'S RAINING !!!")
+    
+    #Gestione durata della pioggia
+    if meteo == "Pioggia":
+        tickpioggia += 1
     
 
-    pos_tartaruga += mosse['tartaruga']
+    mosse = {'tartaruga': muovi_tartaruga(), 'lepre': muovi_lepre()}
+    #Muovo la tartaruga e la lepre
+
+    #Penalità per la pioggia tartaruga
+    if meteo == "Pioggia":
+        pos_tartaruga += (mosse['tartaruga'] - 1)
+    else:
+        pos_tartaruga += mosse['tartaruga']
+
     if pos_tartaruga < 1:
         pos_tartaruga = 1
-    pos_lepre += mosse['lepre']
+    
+    #Penalità per la pioggia lepre
+    if meteo == "Pioggia":
+        pos_lepre += (mosse['lepre'] - 2)
+    else:
+        pos_lepre += mosse['lepre']
+
     if pos_lepre < 1:
         pos_lepre = 1
     
+    #Uscite dalla gara
     if pos_tartaruga >= 70: 
         print("TORTOISE WINS! || VAY!!!")
         break
@@ -62,4 +98,11 @@ while pos_tartaruga < 70 and pos_lepre < 70:
         print("IT'S A TIE.")
         break
     
+    #Stampo le posizioni e aumento il tempo
     visualizza_posizioni(pos_tartaruga, pos_lepre)
+    tick += 1
+    cambiamentometeo = False
+    print(tick)
+
+    #Vedere quando disattivare cambiamento meteo True
+    
