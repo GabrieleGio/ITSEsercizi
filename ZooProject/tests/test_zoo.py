bed1c4a8367385b672dbb2fa381f42ca8e8f3a91
Nio4124 <antoniozottola41204@gmail.com>
2024-05-31 19:15:42 +0200

    test dello zoo

--- ZooProject/tests/test_zoo.py
+++ ZooProject/tests/test_zoo.py
@@ -4,15 +4,17 @@ from src.Zoo import Zoo, ZooKeeper, Animal, Fence
 
 class TestZoo(TestCase):
     def setUp(self) -> None:
-        self.zool: Zoo = Zoo()
-        self.zookeeperl: ZooKeeper = ZooKeeper(name="Gianni", surname="Rossi", id=1)
-        self.fencel: Fence = Fence(area=100, temperature=25.0, habitat="Savana")
-        self.animall: Animal = Animal(name="Pluto", species="Canide", age=5, height=300.0, width=1.0, preferred_habitat="Savana")
+        self.zoo_l: Zoo = Zoo()
+        self.zookeeper_l: ZooKeeper = ZooKeeper(name="Gianni", surname="Rossi", id=1)
+        self.fence_l: Fence = Fence(area=100, temperature=25.0, habitat="Savana")
+        self.animal_l: Animal = Animal(name="Pluto", species="Canide", age=5, height=300.0, width=1.0, preferred_habitat="Savana")
+
     def test_animal_dimension(self):
         self.zookeeper_l.add_animal(self.animal_l, self.fence_l)
         result: int = len(self.fence_l.animals)
         message: str = f"Error: the function add_animal should not add self.animal_l into self.fence_l"
-        self.assertEqual(result, 0, message)
+        self.assertEqual(result, 1, message)
+
     def test_remove(self):
         lenlist:int = len(self.fence_l.animals)
         self.zookeeper_l.remove_animal(self.animal_l, self.fence_l)
